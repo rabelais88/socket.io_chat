86d4f761910286b00a5f5af500cd056b1f77339f
rabelais88 <sungryeolp@gmail.com>
2017-12-10 15:37:58 +0900

    added jquery
    elaborated designs using jquery mobile

--- main.js
+++ main.js
@@ -11,20 +11,29 @@ app.get("/",function(req,res){
 
 //process.env.PORT lets the port be set by Heroku
 http.listen(process.env.PORT || 3000,function(){
-  console.log("server up!");
+  console.log("server is up at port:" + process.env.PORT + "!!!");
 });
 
 io.on("connection", function(socket){
-  console.log("user connected");
+  console.log("a user connected");
   socket.on("disconnect", function(){
-    console.log("user disconnected");
+    console.log("user(" + socket.id +") disconnected");
   });
+
   socket.on("chat message", function(data){
     console.log("a user(" + socket.id + ") in room(" + data.roomname + ")says:" + data.msg);
     socket.to(data.roomname).emit("chat message", data);
   });
-  socket.on("joinroom",function(roomname){
-    socket.join(roomname);
-    console.log("joined room " + roomname);
+
+  socket.on("joinroom",function(data){
+    socket.join(data.roomname);
+    if(data.userid){
+      socket.to(data.roomname).emit("newuser", data);
+    }
+  });
+
+  socket.on("set_userid",function(data){
+    console.log(data.roomname + ": socketid " + socket.id + " set as " + data.userid);
+    socket.to(data.roomname).emit("newuser", data);
   });
 });
